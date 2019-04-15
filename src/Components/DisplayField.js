a693aaa4d77afc8c3cc354a3b998f4cf5e97cdbb
SiimonStark <justinpyktel@gmail.com>
2019-04-15 13:28:05 -0600

    fixed styling) box-sizing: border-box

--- src/Components/DisplayField.js
+++ src/Components/DisplayField.js
@@ -5,17 +5,24 @@ class DisplayField extends Component {
   constructor() {
     super()
     this.state = {
-      gridSquares: ['square-1']
+      gridSquares: ['square-1','square-2','square-3','square-4'],
+      gridContainer: true
     }
   }
 
   render() {
+    const fieldStyle = { display: 'grid', border: '1px solid red', gridTemplateAreas: `"square-1 square-2" "square-3 square-4"` };
     return (
       <main className="DisplayField">
         <div className="grass"></div>
-        <section className="field-container">
-          {this.state.gridSquares.map(square => <div className={square}>{square}</div>)}
-        </section>
+        {  this.state.gridContainer ?
+            <section className="field-container" style={fieldStyle}>
+            {this.state.gridSquares.map(square => <div className={square}>{square}</div>)}
+            </section> :
+            <section className="field-container">
+              {this.state.gridSquares.map(square => <div className={square}>{square}</div>)}
+            </section>
+        }
       </main>
     );
   }
