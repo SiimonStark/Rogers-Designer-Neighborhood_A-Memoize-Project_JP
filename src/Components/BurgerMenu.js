d3dee62d978d86347b291ebce3b0bf726f756e16
SiimonStark <justinpyktel@gmail.com>
2019-04-16 15:30:11 -0600

    finished links & mouseLeave on Hamburger

--- src/Components/BurgerMenu.js
+++ src/Components/BurgerMenu.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 
 class BurgerMenu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  reloadPage = () => {
+    window.location.reload(true);
+  }
+  displayAlert = () => {
+    alert('Feature Coming Soon');
+  }
+
   render() {
+    
     return (
-      <aside className="BurgerMenu">
+      <aside className="BurgerMenu" onMouseLeave={this.props.mouseLeave}>
         <ul>
-          <li>Reset Game</li>
-          <li>Skip Prompt</li>
-          <li>Save Progress</li>
-          <li>Docs: Grid</li>
-          <li>Docs: Flex</li>
+          <li onClick={this.reloadPage}>Reset Game</li>
+          <li onClick={this.displayAlert}>Skip Prompt</li>
+          <li onClick={this.displayAlert}>Save Game</li>
+          <li>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/CSS/grid"
+              target="blank"
+            >
+              Docs: Grid
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/CSS/flex"
+              target="blank"
+            >
+              Docs: Flex
+            </a>
+          </li>
         </ul>
       </aside>
     );
